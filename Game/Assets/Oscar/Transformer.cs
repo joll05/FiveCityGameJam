07b4c644ee7767933f89bd58bef67f6d638b6fc4
Oscar Lundberg <info@oscarlundberg.com>
2019-11-23 14:27:40 +0100

    Remove library

---
+++ Game/Assets/Oscar/Transformer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transformer : MonoBehaviour
+{
+    public GameObject state1;
+    public GameObject state2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnHit()
+    {
+        state1.SetActive(false);
+        state2.SetActive(true);
+    }
+}
