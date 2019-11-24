dea36487edc88d7ca137619d959e3175844cd3dd
joll05 <44553291+joll05@users.noreply.github.com>
2019-11-24 14:26:43 +0100

    Game loads after intro

---
+++ Game/Assets/LoadSceneAfterDelay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneAfterDelay : MonoBehaviour
+{
+    public float Delay = 15f;
+
+    public int scene;
+
+    void Start()
+    {
+        Invoke("Load", Delay);
+    }
+
+    void Load()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
