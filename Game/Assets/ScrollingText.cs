506aebd6ccded2f58f41ab58c4828a61c35b1e0a
joll05 <44553291+joll05@users.noreply.github.com>
2019-11-24 15:06:10 +0100

    Credits

---
+++ Game/Assets/ScrollingText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingText : MonoBehaviour
+{
+    public float Scrollspeed = 100;
+
+    void Update()
+    {
+        transform.Translate(0, Scrollspeed * Time.deltaTime, 0);
+    }
+}
