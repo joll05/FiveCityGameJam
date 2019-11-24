9b30c6a636727fb0683791ef8ddab89cf1423472
BuildTools <unconfigured@null.spigotmc.org>
2019-11-24 11:55:21 +0100

    Fixes

--- Game/Assets/Scripts/Level.cs
+++ Game/Assets/Scripts/Level.cs
@@ -5,29 +5,4 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
     public float lvl;
-    //GameObject cameraMain;
-    public static Camera current;
-    CameraScript cameraScript;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        cameraScript = Camera.main.gameObject.GetComponent<CameraScript>();
-        current = Camera.main;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        if (cameraScript.currentLvl == lvl)
-        {
-            this.gameObject.SetActive(true);
-        }
-        else
-        {
-            this.gameObject.SetActive(false);
-        }
-    }
 }
