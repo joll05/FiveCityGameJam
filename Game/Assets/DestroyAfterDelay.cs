de92728569f877f221b9bfd478ab001723e08598
joll05 <44553291+joll05@users.noreply.github.com>
2019-11-24 11:50:52 +0100

    Intro

---
+++ Game/Assets/DestroyAfterDelay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DestroyAfterDelay : MonoBehaviour
+{
+    public float Time = 1;
+
+    void Start()
+    {
+        Invoke("DestroySelf", Time);
+    }
+
+    void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
