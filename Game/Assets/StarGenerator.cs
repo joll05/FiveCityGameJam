86b575b405c8bbc1a05b40781eb6151f87cdc081
joll05 <44553291+joll05@users.noreply.github.com>
2019-11-24 15:35:29 +0100

    Stars in intro

---
+++ Game/Assets/StarGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarGenerator : MonoBehaviour
+{
+    public int Count = 30;
+
+    public GameObject star;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), Random.Range(-20, 20));
+            Instantiate(star, position, Quaternion.identity);
+        }
+
+        Destroy(gameObject);
+    }
+}
